f49ca306bf5bf372436ccad895146968acb44e67
mdedeu <mdedeu@itba.edu.ar>
2020-12-05 17:06:29 -0300

    minor fixes

--- src/game/backend/level/Level2.java
+++ src/game/backend/level/Level2.java
@@ -1,5 +1,10 @@
 package game.backend.level;
 
-public class Level2 extends Level {
+import game.backend.GameState;
 
+public class Level2 extends Level{
+    @Override
+    protected GameState newState() {
+        return null;
+    }
 }
