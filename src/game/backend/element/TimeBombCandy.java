55f1a3204ad55c6ea57bb69518099acfd19ab8ab
robrodriguez99 <64867075+robrodriguez99@users.noreply.github.com>
2020-12-05 09:14:03 -0300

    time clases

--- src/game/backend/element/TimeBombCandy.java
+++ src/game/backend/element/TimeBombCandy.java
@@ -2,4 +2,7 @@ package game.backend.element;
 
 public class TimeBombCandy extends TimeCandy {
 
+    public TimeBombCandy(CandyColor color, int time) {
+        super(color, time);
+    }
 }
--- src/game/backend/element/TimeBonusCandy.java
+++ src/game/backend/element/TimeBonusCandy.java
@@ -2,4 +2,7 @@ package game.backend.element;
 
 public abstract class TimeBonusCandy extends TimeCandy {
 
+    public TimeBonusCandy(CandyColor color, int time) {
+        super(color, time);
+    }
 }
