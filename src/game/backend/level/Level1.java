075c38e7c698cc2fe946c07775bd055565a15d31
mdedeu <mdedeu@itba.edu.ar>
2020-12-05 11:58:48 -0300

    borrado de método

--- src/game/backend/level/Level1.java
+++ src/game/backend/level/Level1.java
@@ -28,14 +28,6 @@ public class Level1 extends Level {
 		}
 		return ret;
 	}
-
-
-	//DESPUES HAY QUE BORRAR ESTE METODO
-	@Override
-	public void nothing() {
-
-	}
-
 	private class Level1State extends GameState {
 		private long requiredScore;
 		private long maxMoves;
