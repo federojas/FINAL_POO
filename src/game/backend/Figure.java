bc40be5fa59cc5733c8030765216be51031ec92c
mdedeu <mdedeu@itba.edu.ar>
2020-12-07 19:23:51 -0300

    Figure Class Consistency

--- src/game/backend/Figure.java
+++ src/game/backend/Figure.java
@@ -12,20 +12,20 @@ import java.awt.Point;
 
 public enum Figure {
 	
-	F6(new Point[]{ new Point(0,-2), new Point(0,-1), new Point(0,1), new Point(0,2)}, 240, Bomb.class, false),
-	F15(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(1,0), new Point(2,0)}, 15, Bomb.class, false),	
-	F4(new Point[]{ new Point(0,-1), new Point(0,1), new Point(0,2)}, 112,  VerticalStripedCandy.class),
-	F5(new Point[]{ new Point(0,-2), new Point(0,-1), new Point(0,1)}, 208, VerticalStripedCandy.class),
-	F13(new Point[]{ new Point(-1,0), new Point(1,0), new Point(2,0)}, 13,  HorizontalStripedCandy.class),	
-	F14(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(1,0)}, 7, HorizontalStripedCandy.class),
-	F7(new Point[]{ new Point(0,1), new Point(0,2), new Point(1,0), new Point(2,0)}, 60, WrappedCandy.class),
-	F8(new Point[]{ new Point(0,-1), new Point(0,1), new Point(1,0), new Point(2,0)}, 92, WrappedCandy.class),
-	F9(new Point[]{ new Point(0,-2), new Point(0,-1), new Point(1,0), new Point(2,0)}, 204, WrappedCandy.class),
-	F16(new Point[]{ new Point(-1,0), new Point(1,0), new Point(0,1), new Point(0,2)}, 53, WrappedCandy.class),
-	F17(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(0,1), new Point(0,2)}, 51, WrappedCandy.class),
-	F18(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(0,-1), new Point(0,-2)}, 195, WrappedCandy.class),
-	F19(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(0,-1), new Point(0,1)}, 83, WrappedCandy.class),
-	F20(new Point[]{ new Point(-1,0), new Point(1,0), new Point(0,-2), new Point(0,-1)}, 197, WrappedCandy.class),
+	F6(new Point[]{ new Point(0,-2), new Point(0,-1), new Point(0,1), new Point(0,2)}, 240, new Bomb(), false),
+	F15(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(1,0), new Point(2,0)}, 15, new Bomb(), false),
+	F4(new Point[]{ new Point(0,-1), new Point(0,1), new Point(0,2)}, 112,   new VerticalStripedCandy()),
+	F5(new Point[]{ new Point(0,-2), new Point(0,-1), new Point(0,1)}, 208, new VerticalStripedCandy()),
+	F13(new Point[]{ new Point(-1,0), new Point(1,0), new Point(2,0)}, 13,  new HorizontalStripedCandy()),
+	F14(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(1,0)}, 7,  new HorizontalStripedCandy()),
+	F7(new Point[]{ new Point(0,1), new Point(0,2), new Point(1,0), new Point(2,0)}, 60, new WrappedCandy()),
+	F8(new Point[]{ new Point(0,-1), new Point(0,1), new Point(1,0), new Point(2,0)}, 92, new WrappedCandy()),
+	F9(new Point[]{ new Point(0,-2), new Point(0,-1), new Point(1,0), new Point(2,0)}, 204, new WrappedCandy()),
+	F16(new Point[]{ new Point(-1,0), new Point(1,0), new Point(0,1), new Point(0,2)}, 53, new WrappedCandy()),
+	F17(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(0,1), new Point(0,2)}, 51, new WrappedCandy()),
+	F18(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(0,-1), new Point(0,-2)}, 195,new WrappedCandy()),
+	F19(new Point[]{ new Point(-2,0), new Point(-1,0), new Point(0,-1), new Point(0,1)}, 83,new WrappedCandy()),
+	F20(new Point[]{ new Point(-1,0), new Point(1,0), new Point(0,-2), new Point(0,-1)}, 197, new WrappedCandy()),
 	F1(new Point[]{new Point(0,1), new Point(0,2)}, 48),
 	F2(new Point[]{new Point(0,-1), new Point(0,1)}, 80),
 	F3(new Point[]{new Point(0,-1), new Point(0,-2)}, 192),
@@ -36,26 +36,26 @@ public enum Figure {
 	
 	private Point[] points;
 	private int value;
-	private Class<?> replacementClass;
+	private Element replacement;
 	private boolean isCandyRepl = true;
 
-	Figure(Point[] points, int value, Class<?> replacementClass) {
+	Figure(Point[] points, int value, Element replacementClass) {
 		this.points = points;
 		this.value = value;
-		this.replacementClass = replacementClass;
+		this.replacement = replacementClass;
 	}
 	
-	Figure(Point[] points, int value, Class<?> replacementClass, boolean isCandyRepl) {
+	Figure(Point[] points, int value, Element replacementClass, boolean isCandyRepl) {
 		this.points = points;
 		this.value = value;
-		this.replacementClass = replacementClass;
+		this.replacement = replacementClass;
 		this.isCandyRepl = isCandyRepl;
 	}
 	
 	Figure(Point[] points, int value) {
 		this.points = points;
 		this.value = value;
-		this.replacementClass = null;
+		this.replacement = null;
 	}
 	
 	public Point[] getPoints() {
@@ -67,7 +67,7 @@ public enum Figure {
 	}
 
 	public boolean hasReplacement() {
-		return replacementClass != null;
+		return replacement != null;
 	}
 	
 	public boolean matches(int acum) {
@@ -75,15 +75,10 @@ public enum Figure {
 	}
 	
 	public Element generateReplacement(CandyColor color) {
-		try {
-			Element e;
-			e = (Element) replacementClass.newInstance();
 			if (isCandyRepl) {
-				((Candy)e).setColor(color);
+				((Candy)replacement).setColor(color);
 			} 
-			return e;
-		} catch(Exception e) {
-		}
-		return null;
+			return replacement;
+
 	}	
 }
